dbcf8763919d45b08defcecb0919f34c257f7642
Myriam <myriam@halfapx.com>
2018-06-26 17:59:39 +0200

    iterate through directory

---
+++ assets/js/dashboard.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const shell = require('shelljs')
+const child_process = require('child_process')
+const prefs = require('./prefs')
+let store = prefs.store
+
+const {
+    ipcRenderer,
+    remote
+} = require('electron');
+
+const pageContent = document.querySelector('.container')
+const popupContent = document.querySelector('.popup .content-loader')
+const functions = require("./functions.js")
+let jekyllFiles = functions.getPathsInDir(store.get('currentProjectPath').toString())
+
+console.log(jekyllFiles);
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,13 @@
-"use strict";
-
-const path = require('path');
+"use strict"
+const fs = require('fs')
+const path = require('path')
 
 // build HTML Path for given file
 module.exports.htmlPath = function htmlPath(filename) {
     return path.join(__dirname, '..', 'html', filename+'.html');
 }
 
+// open Popup function
 module.exports.openPopup = function openPopup() {
     const body = document.querySelector('body')
     body.classList.add('popup-open')
@@ -15,6 +16,20 @@ module.exports.openPopup = function openPopup() {
     body.appendChild(behindPopup)
     const popup = document.querySelector('.popup')
     popup.style.display = 'block'
+}
+
+// List all Files in Given Directory
+module.exports.getPathsInDir = function getPathsInDir(dir, filelist = []) {
+    fs.readdirSync(dir).forEach(file => {
+        const dirFile = path.join(dir, file);
+        try {
+            filelist = getPathsInDir(dirFile, filelist);
+        } catch (err) {
+            if (err.code === 'ENOTDIR' || err.code === 'EBUSY') filelist = [...filelist, dirFile];
+            else throw err;
+        }
+    });
+    return filelist;
 }
 
 // Styling Input functionality, has to be exported due to being reloaded on dynamically created elements
